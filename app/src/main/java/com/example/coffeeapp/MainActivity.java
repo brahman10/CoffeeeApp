ad7ee3f45e78dcea5077d90b6b70e3f2123b6501
Yash <yashchaturvedi10122000@gmail.com>
2020-05-25 23:14:49 +0530

    2.0

--- app/src/main/java/com/example/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/coffeeapp/MainActivity.java
@@ -6,15 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
+    int n=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
     public void submitOrder(View view) {
-        display(1);
+
+
+        displayPrice(n*5);
+    }
+    public void Decrement(View view) {
+        if(n==0){
+            display(0);
+        }
+        else{
+            n=n-1;
+            display(n);
+        }
+    }
+    public void Increment(View view) {
+
+        n=n+1;
+        display(n);
+
     }
 
     /**
@@ -23,5 +46,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.order);
         quantityTextView.setText("" + number);
+    }
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.txtprice);
+        TextView price = (TextView) findViewById(R.id.price);
+        price.setVisibility(View.VISIBLE);
+        priceTextView.setVisibility(View.VISIBLE);
+        priceTextView.setText("PRICE");
+        price.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }

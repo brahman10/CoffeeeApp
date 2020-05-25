1d4fa47f714ff0afc2d00473e65e58a36008fbd0
Yash <yashchaturvedi10122000@gmail.com>
2020-05-25 17:05:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/coffeeapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.coffeeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.order);
+        quantityTextView.setText("" + number);
+    }
+}
